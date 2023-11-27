cca4daa0b66b8a628eca42fe84fe6a6b3df58da2
Jakub Roman <jakub@jakubroman.sk>
2023-11-27 14:53:12 +0100

    reduce git verbosity

--- bin/install.sh
+++ bin/install.sh
@@ -4,13 +4,13 @@ if [ ! -d ~/.liquidprompt ]; then
 
   log_info "Installing liquidprompt..."
 
-  git clone --branch stable https://github.com/nojhan/liquidprompt ~/.liquidprompt || exit 1
+  git clone --quiet --branch stable https://github.com/nojhan/liquidprompt ~/.liquidprompt || exit 1
 
 else
 
   log_info "Updating liquidprompt..."
 
   cd ~/.liquidprompt || exit 1
-  git pull || exit 1
+  git pull --quiet || exit 1
 
 fi
